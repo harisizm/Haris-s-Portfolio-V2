b99e5ccfee58319c694e6871f832d3b8f74eac75
harisizm <120340766+harisizm@users.noreply.github.com>
2025-06-26 03:26:01 +0500

    FEATURED PROJECTS SECTION+ TAPE

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -9,9 +9,9 @@ import { HeroOrbit } from '@/components/HeroOrbit';
 export const HeroSection = () => {
   return( 
 
-  <div className='py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip'>
-    <div className='absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]'>
-    <div className='absolute inset-0 -z-30 opacity-5' style={{
+  <div className='py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip '>
+    <div className='absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)] '>
+    <div className='absolute inset-0 -z-30 opacity-5 ' style={{
       backgroundImage: `url(${grainIamge.src})`
     }}></div>
     <div className='size-[620px] hero-ring '></div>
@@ -49,16 +49,16 @@ export const HeroSection = () => {
       <div className='size-2 rounded-full bg-emerald-300/20'/>
     </HeroOrbit>
     </div>
-    <div className="container">
-      <div className='flex flex-col items-center'>
+    <div className="container ">
+      <div className='flex flex-col items-center '>
       <Image src={memojiImage} className="size-[100px]" alt="person peeking from behind laptop"/>
-      <div className='bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg'>
+      <div className='bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg '>
         <div className='bg-green-500 size-2.5 rounded-full'></div>
         <div className='text-sm font-medium'>Available for new projects</div>
       </div>
       </div>
-      <div className='max-w-lg mx-auto'>
-      <h1 className='font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide'>Building Exceptional User Experiences</h1>
+      <div className='max-w-lg mx-auto '>
+      <h1 className='font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide '>Building Exceptional User Experiences</h1>
       <p className='mt-4 text-center text-white/60 md:text-lg'>I specialize in transforming designs into functional, high-performing web applications. Let's discuss your next project.
       </p>
       </div>
