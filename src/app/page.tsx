4c92337927cddf29b31266a0b80a38a24880d6ad
harisizm <120340766+harisizm@users.noreply.github.com>
2025-06-26 22:15:59 +0500

    testimonials and about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import { Header } from "@/sections/Header";
 import { HeroSection } from "@/sections/Hero";
 import { ProjectsSection } from "@/sections/Projects";
 import { TapeSection } from "@/sections/Tape";
+import { TestimonialsSection } from "@/sections/Testimonials";  
+import { AboutSection } from "@/sections/About";  
+
 
 export default function Home() {
   return (
@@ -10,6 +13,8 @@ export default function Home() {
      <HeroSection/>
      <ProjectsSection/>
      <TapeSection/>
+     <TestimonialsSection/>
+     <AboutSection/>
     </div>
   );
 }
--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,3 +1,68 @@
+import { SectionHeader } from "@/components/SectionHeader";
+import { Card } from "@/components/Card";
+import StarIcon from '@/assets/icons/star.svg';
+import bookImage from '@/assets/images/book-cover.png';
+import Image from "next/image";
+import { title } from "process";
+import javascriptIcon from '@/assets/icons/square-js.svg'
+import HTMLIcon from '@/assets/icons/html5.svg'
+import CssIcon from '@/assets/icons/css3.svg'
+import ReactIcon from '@/assets/icons/react.svg'
+import ChromeIcon from '@/assets/icons/chrome.svg'
+
+const toolboxItems = [
+  {
+    title: 'JavaScript',
+    icon: '',
+  },
+  {
+    title: 'HTML5',
+    icon: '',
+  },
+  {
+    title: 'CSS3',
+    icon: '',
+  },
+  {
+    title: 'React',
+    icon: '',
+  },
+  {
+    title: 'Github',
+    icon: '',
+  },
+  
+]
+
 export const AboutSection = () => {
-  return <div>About Section</div>;
+  return (
+  <div className="pb-96">
+    <SectionHeader 
+    eyebrow="About Me"
+     title="A Glimpse Into My World"
+     description="Learn more about who I am, what I do, and what inspires me."/>
+     <div>
+      <Card>
+        <div>
+          <StarIcon/>
+          <h3>My Reads</h3>
+          <p>Explore the books shaping my perspectives.</p>
+        </div>
+        <Image src={bookImage} alt="book cover"/>
+      </Card>
+      <Card>
+        <div>
+          <StarIcon/>
+          <h3>My Toolbox</h3>
+          <p>
+            Explore the technologies and tools I use to craft exceptional digital experiences.
+            </p>
+        </div>
+        <div>
+
+        </div>
+      </Card>
+     </div>
+  </div>
+  );
 };
